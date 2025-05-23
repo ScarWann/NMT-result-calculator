8950fa57f502a9d2bb3fd4b3f84081661ffaba8d
ScarWann <lyosha_keliukh@outlook.com>
2025-05-23 23:24:46 +0300

    Added standard deviation calculation

--- estimator.py
+++ estimator.py
@@ -39,15 +39,21 @@ PHYSICS_RESULTS_TABLE = {
 
 fluct = 0
 
-def pmf(p, n, k):
+def EValue(plist: list, vlist: list):
+    return sum([plist[i]*vlist[i] for i in range(len(plist))])
+
+def standard_deviation(plist: list, vlist: list, mean, f = lambda x, mean: (x - mean)**2):
+    return math.sqrt(sum([plist[i]*f(vlist[i], mean) for i in range(len(plist))]))
+
+def bf(p, n, k):
     return math.comb(n, k)*(p**k)*((1-p)**(n-k))
 
 def wbd(n, k):
-    distribution = np.array([pmf(i/n, n, i) * pmf(i/n, n, k) for i in range(n+1)])
+    distribution = np.array([bf(i/n, n, k) for i in range(n+1)])
     return  distribution / sum(distribution)
 
 def bd(p, n):
-    return [pmf(p, n, i) for i in range(n+1)]
+    return [bf(p, n, i) for i in range(n+1)]
 
 def bdsum(plist, n):
     bds = [np.array(bd(i/n, n))*plist[i] for i in range(n+1)]
@@ -93,12 +99,12 @@ class Estimator(Scene):
 
         probabilities_distribution = condense(wbd(trials, successes), blocks)
 
-        probability_text = Text("Knownledge estimates", font_size = 28)
+        probability_text = Text("Knowledge estimates", font_size = 28)
         probability_mean = sum([probabilities_distribution[i]*i for i in range(33)])
-        probability_fail = sum(probabilities_distribution[:5])
+        probability_variance = standard_deviation(probabilities_distribution, [i for i in range(len(PHYSICS_RESULTS_TABLE))], probability_mean)
         probability_perfect = probabilities_distribution[-1]
         probability_mean_text = Text(f"Mean: {PHYSICS_RESULTS_TABLE[math.floor(probability_mean)]:.2f}", font_size = 20)
-        probability_fail_text = Text(f"Failure chance: {probability_fail*100:.2f}%", font_size = 20)
+        probability_variance_text = Text(f"Standard deviation: {probability_variance:.2f}", font_size = 20)
         probability_perfect_text = Text(f"Perfect chance: {probability_perfect*100:.2f}%", font_size = 20)
         probability_chart = BarChart(
             values=probabilities_distribution,
@@ -121,16 +127,16 @@ class Estimator(Scene):
                 top_probabilities[-1].align_to(top_probabilities[-2], RIGHT)
         probability_mean_text.next_to(probability_chart, UP)
         probability_mean_text.align_to(probability_chart, LEFT)
-        probability_fail_text.next_to(probability_mean_text, UP)
-        probability_fail_text.align_to(probability_mean_text, LEFT)
-        probability_perfect_text.next_to(probability_fail_text, UP)
-        probability_perfect_text.align_to(probability_fail_text, LEFT)
+        probability_variance_text.next_to(probability_mean_text, UP)
+        probability_variance_text.align_to(probability_mean_text, LEFT)
+        probability_perfect_text.next_to(probability_variance_text, UP)
+        probability_perfect_text.align_to(probability_variance_text, LEFT)
         probability_text.next_to(probability_perfect_text, UP)
         probability_text.align_to(probability_chart, RIGHT)
         self.play(Write(probability_text))
         self.add(probability_chart)
         self.play(Write(probability_mean_text))
-        self.play(Write(probability_fail_text))
+        self.play(Write(probability_variance_text))
         self.play(Write(probability_perfect_text))
         for e in top_probabilities:
             self.play(Write(e))
@@ -138,10 +144,10 @@ class Estimator(Scene):
         final_text = Text("Real result estimates", font_size = 28)
         final_estimate = bdsum(probabilities_distribution, 32)
         final_mean = sum([final_estimate[i]*i for i in range(33)])
-        final_fail = sum(final_estimate[:5])
+        final_variance = standard_deviation(final_estimate, [i for i in range(len(PHYSICS_RESULTS_TABLE))], final_mean)
         final_perfect = final_estimate[-1]
         final_mean_text = Text(f"Mean: {PHYSICS_RESULTS_TABLE[math.floor(final_mean)]:.2f}", font_size = 20)
-        final_fail_text = Text(f"Failure chance: {final_fail*100:.2f}%", font_size = 20)
+        final_variance_text = Text(f"Standard deviation: {final_variance:.2f}", font_size = 20)
         final_perfect_text = Text(f"Perfect chance: {final_perfect*100:.2f}%", font_size = 20)
         final_chart = BarChart(
             values=final_estimate,
@@ -164,16 +170,16 @@ class Estimator(Scene):
                 top_finals[-1].align_to(top_finals[-2], RIGHT)
         final_mean_text.next_to(final_chart, UP)
         final_mean_text.align_to(final_chart, LEFT)
-        final_fail_text.next_to(final_mean_text, UP)
-        final_fail_text.align_to(final_mean_text, LEFT)
-        final_perfect_text.next_to(final_fail_text, UP)
-        final_perfect_text.align_to(final_fail_text, LEFT)
+        final_variance_text.next_to(final_mean_text, UP)
+        final_variance_text.align_to(final_mean_text, LEFT)
+        final_perfect_text.next_to(final_variance_text, UP)
+        final_perfect_text.align_to(final_variance_text, LEFT)
         final_text.next_to(final_perfect_text, UP)
         final_text.align_to(final_chart, RIGHT)
         self.play(Write(final_text))
         self.add(final_chart)
         self.play(Write(final_mean_text))
-        self.play(Write(final_fail_text))
+        self.play(Write(final_variance_text))
         self.play(Write(final_perfect_text))
         for e in top_finals:
             self.play(Write(e))
